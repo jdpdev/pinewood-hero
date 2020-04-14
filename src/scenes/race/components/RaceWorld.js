c01e9bf74bd3fdc80322c20f396b5708a8de9e8b
Jason DuPertuis <gallahad@me.com>
2020-04-14 10:16:35 -0400

    show race distances

--- src/scenes/race/components/RaceWorld.js
+++ src/scenes/race/components/RaceWorld.js
@@ -6,18 +6,39 @@ const X_STEP = TILE_WIDTH / 2;
 const Y_STEP = TILE_HEIGHT / 2;
 const H_STEP = 25;
 
+const distanceMarkerStyle = {
+    fontFamily: '"Noto Sans", Futura, sans-serif',
+    color: '#aaa',
+    align: 'right',
+    fixedWidth: 30
+}
+
 export class RaceWorld extends Phaser.GameObjects.Container {
     constructor(scene) {
         super(scene);
     }
 
-    placeTile(sprite, x, y, height) {
-        const tileX = (x - y) * X_STEP;
-        const tileY = ((x + y) * Y_STEP) - (height * H_STEP);
-        const tile = this.scene.add.image(tileX, tileY, sprite);
+    placeTile(sprite, tileX, tileY, height) {
+        const {x, y} = this.getRealCoordinate(tileX, tileY, height);
+        const tile = this.scene.add.image(x, y, sprite);
 
         tile.setDisplaySize(TILE_WIDTH, TILE_WIDTH);
 
         return tile;
+    }
+
+    placeDistanceMarker(label, tileX, tileY, height) {
+        const {x, y} = this.getRealCoordinate(tileX, tileY, height);
+        const text = this.scene.add.text(x - 30, y - 10, label, distanceMarkerStyle);
+        text.angle = -30;
+
+        return text;
+    }
+
+    getRealCoordinate(tileX, tileY, height) {
+        const x = (tileX - tileY) * X_STEP;
+        const y = ((tileX + tileY) * Y_STEP) - (height * H_STEP);
+
+        return {x , y};
     }
 }
--- src/scenes/race/components/Track.js
+++ src/scenes/race/components/Track.js
@@ -16,12 +16,18 @@ export default class Track {
             this.tiles.push([]);
         }
 
+        // track tiles
         for (let x = 0; x < extendLength; x++) {
             for (let y = 0; y < lanes; y++) {
                 const tile = this.getTrackTile(x, y, length);
                 const height = Math.max(length - x, 1);
                 this.tiles[x].push(this.world.placeTile(tile, x, y, height));
             }  
+        }
+
+        // distance markers
+        for (let x = 4; x < length; x += 5) {
+            this.world.placeDistanceMarker(x + 1, x, lanes, length - x);
         }
 
         this._winningDistance = (this.length - 2) * (709.94 * this.tiles[0][0].scaleX);
